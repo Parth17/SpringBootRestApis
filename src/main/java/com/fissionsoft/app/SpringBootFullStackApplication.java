b4aa1648cca450f78d569d8bbb7edeafc2e3ed75
Parth <mr.parthmehta@gmail.com>
2019-07-23 12:59:38 +0530

    Initial commit.

---
+++ src/main/java/com/fissionsoft/app/SpringBootFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.fissionsoft.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFullStackApplication.class, args);
+	}
+
+}
